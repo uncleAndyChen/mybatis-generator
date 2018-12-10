5e94f121310e9da2a3aa50539bfcdd1037ef7ef6
AndyChen <andy@lovesofttech.com>
2018-12-10 23:25:06 +0800

    - 2018-12-09     - 将 MBG 版本由 1.3.5 升级至当前最新版 1.3.7。     - 将 mybatis 由 3.4.1 升级至 3.4.6。     - 修改和完善本说明文档。     - 重构生成表配置的类：`CreateTablePropertyService`，优化完善了相关代码、添加数据库的各种配置信息，更易于使用和维护。     - 将生成表配置的类`CreateTablePropertyService`单独提取至一个独立的项目，以尽可能少的修改官方项目`mybatis-generator-core`源码。     - 重命名model与dal模块名，更易于理解。     - 在根目录添加 pom.xml，方便 IDEA 通过该文件直接导入。

---
+++ create-table-property/README.md
@@ -0,0 +1,3 @@
+# 连接MySQL生成MBG需要的表配置信息
+1. 可以通过本 module（IDEA 的叫法，如果用 Eclipse，叫 Project），学习 spring boot 2.1.1 获取 application.yml 配置信息。
+1. 学习如何直接连接数据库并读取到数据库的所有表名，以及每张表的主键。
---
+++ create-table-property/src/main/java/org/mybatis/generator/property/CreateTablePropertyController.java
@@ -0,0 +1,33 @@
+package org.mybatis.generator.property;
+
+import org.mybatis.generator.property.config.DatabaseConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CreateTablePropertyController {
+    @Autowired
+    private DatabaseConfig databaseConfig;
+
+    @RequestMapping("/getDatabaseConfig")
+    public DatabaseConfig getDatabaseConfig(){
+        return databaseConfig;
+    }
+
+    @RequestMapping("/getTableProperties")
+    public StringBuilder getTableProperties(){
+        try {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("<textarea rows=\"30\" cols=\"248\">");
+            sb.append(CreateTablePropertyService.getTableProperties(databaseConfig));
+            sb.append("</textarea>");
+
+            return sb;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
